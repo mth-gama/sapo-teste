7bfdef71a834a933643c2dd00c5a1a1b9386921a
Matheus Gama <matt.backup821@gmail.com>
2025-11-16 18:34:25 +0000

    :up: Update poject

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
-import requests
+import requests, os
 import pprint
 
 from django.views.generic import TemplateView
+from dotenv import load_dotenv
 
+load_dotenv()
 
 
 pp = pprint.PrettyPrinter()
@@ -10,7 +12,8 @@ class IndexPageView(TemplateView):
     template_name = 'main/index.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=c75ad3b91cbb490daeea9bae96348f2f'
+        apikey_newsapi = os.getenv("APIKEY_NEWSAPI")
+        url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={apikey_newsapi}'
         response = requests.get(url)
         data = response.json()
         articles = data.get('articles', []) if response.status_code == 200 else []
